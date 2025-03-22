41d9e37dcf94ea705f837ed08121b1c8c6495ac1
Abdul Hayy Khan <134889827+abdulhayykhan@users.noreply.github.com>
2025-03-23 00:14:37 +0500

    Update Random Joke Generator.py

--- Random Joke Generator.py
+++ Random Joke Generator.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import time
 
 def get_joke():
     url = "https://v2.jokeapi.dev/joke/Any?type=twopart"
@@ -12,10 +13,23 @@ def get_joke():
     else:
         return "Error fetching joke", ""
 
+st.set_page_config(page_title="Random Joke Generator", page_icon="🤣")
 st.title("🤣 Random Joke Generator 😂")
-st.write("Click the button below to get a random joke!")
+st.write("Click the button below to get a random joke and have a good laugh!")
 
-if st.button("Get a Joke!"):
-    setup, delivery = get_joke()
-    st.write("**" + setup + "**")
-    st.write("😆 " + delivery)
+# Adding a fun emoji separator
+st.markdown("---")
+
+if st.button("Get a Joke! 😂"):
+    with st.spinner("Fetching a hilarious joke for you..."):
+        time.sleep(1.5)  # Simulating a small delay for better engagement
+        setup, delivery = get_joke()
+    
+    st.success("Here’s your joke:")
+    st.write(f"**{setup}**")
+    time.sleep(1)  # Small pause before punchline
+    st.write(f"😆 **{delivery}**")
+    
+    # Adding an option to get another joke
+    st.markdown("---")
+    st.write("Want another joke? Click the button again! 🎭")
