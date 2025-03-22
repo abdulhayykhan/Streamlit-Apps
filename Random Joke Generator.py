466c284c3296c002db5024ac424165af881ff636
Abdul Hayy Khan <134889827+abdulhayykhan@users.noreply.github.com>
2025-03-23 00:42:11 +0500

    Update Random Joke Generator.py

--- Random Joke Generator.py
+++ Random Joke Generator.py
@@ -17,19 +17,17 @@ st.set_page_config(page_title="Random Joke Generator", page_icon="🤣")
 st.title("🤣 Random Joke Generator 😂")
 st.write("Click the button below to get a random joke and have a good laugh!")
 
-# Adding a fun emoji separator
 st.markdown("---")
 
 if st.button("Get a Joke! 😂"):
     with st.spinner("Fetching a hilarious joke for you..."):
-        time.sleep(1.5)  # Simulating a small delay for better engagement
+        time.sleep(1.5)  
         setup, delivery = get_joke()
     
     st.success("Here’s your joke:")
     st.write(f"**{setup}**")
-    time.sleep(1)  # Small pause before punchline
+    time.sleep(1)  
     st.write(f"😆 **{delivery}**")
     
-    # Adding an option to get another joke
     st.markdown("---")
     st.write("Want another joke? Click the button again! 🎭")
